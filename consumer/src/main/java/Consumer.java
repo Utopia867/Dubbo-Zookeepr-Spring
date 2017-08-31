ca3a126dc66e9019fd9863278df4a9e67846b188
kingflag <1362168862@qq.com>
2017-08-31 20:55:16 +0800

    consumer

---
+++ consumer/src/main/java/Consumer.java
@@ -0,0 +1,16 @@
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import com.alibaba.dubbo.demo.DemoService;
+
+public class Consumer {
+    public static void main(String[] args) throws Exception {
+        /*ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(new String[] {"META-INF/spring/dubbo-demo-consumer.xml"});
+        context.start();*/
+    	ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(new String[] {"dubbo-demo-consumer.xml"});
+        context.start();
+
+        DemoService demoService = (DemoService)context.getBean("demoService"); // 获取远程服务代理
+        String hello = demoService.sayHello("world"); // 执行远程方法
+
+        System.out.println( hello ); // 显示调用结果
+    }
+}
